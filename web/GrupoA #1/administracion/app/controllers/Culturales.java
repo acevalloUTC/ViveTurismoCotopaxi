50b56dd7bfd1780a19f5a94f490ff34577b7f99c
echangoUTC <evelynforever00@outlook.com>
2016-11-06 01:44:08 -0500

    GrupoAechangoUTC#3
    ingreso de los datos a la base de datos

---
+++ web/GrupoA #1/administracion/app/controllers/Culturales.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+import models.Cultural;
+public class Culturales extends Controller {
+
+	 public static void index() {
+	        render();
+	    }
+	public static void culturales(){
+	    	render();
+	    }
+	
+public static void guardar(String nombre,String descripcion,String ubicacion,String imagen){
+		
+	Cultural cultura=new Cultural(nombre,descripcion,ubicacion,imagen);
+	cultura.save();	
+}
+}
--- web/GrupoA #1/administracion/app/models/Cultural.java
+++ web/GrupoA #1/administracion/app/models/Cultural.java
@@ -6,19 +6,18 @@ import play.db.jpa.Model;
 @Entity
 
 public class Cultural extends Model{
-	public String Id;
-	public String Nombre;
-	public String Descripcion;
-	public String Ubicacion;
-	public String Imagen;
 	
-	public Cultural(String id, String nombre, String descripcion,String ubicacion, String imagen) {
+	public String nombre;
+	public String descripcion;
+	public String ubicacion;
+	public String imagen;
+	
+	public Cultural(String nombre, String descripcion,String ubicacion, String imagen) {
 		super();
-		this.Id = id;
-		this.Nombre = nombre;
-		this.Descripcion = descripcion;
-		this.Ubicacion = ubicacion;
-		this.Imagen = imagen;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.ubicacion = ubicacion;
+		this.imagen = imagen;
 	}
 
 }
