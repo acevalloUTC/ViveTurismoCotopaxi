f91c415016d4f2c8488ee9b367acb7ee5e877f46
aguanoquUTC <ana.guanoquiza07@gmail.com>
2016-11-06 14:54:21 -0500

    Se lleno las listas de las parroquias
    Se configuro mediante codigo la lista para que muestre las parroquias
    del cantón Latacunga

--- mobile/GrupoH/DivisionPoliticaProyecto/app/src/main/java/com/divisionpoliticaproyecto/Latacunga.java
+++ mobile/GrupoH/DivisionPoliticaProyecto/app/src/main/java/com/divisionpoliticaproyecto/Latacunga.java
@@ -5,8 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Latacunga extends AppCompatActivity {
+
+    ListView listaPU, listaPR;
+    ArrayAdapter<String> adaptadorPU;
+    ArrayAdapter<String> adaptadorPR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,37 @@ public class Latacunga extends AppCompatActivity {
         //con estas lineas se visualiza el boton atras de la parte superior izquierda
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        listaPU = (ListView)findViewById(R.id.lstPU);
+        listaPR = (ListView)findViewById(R.id.lstPR);
+
+        String[] PU = new String[]{
+                "La Matriz",
+                "Eloy Alfaro (San Felipe)",
+                "Ignacio Flores (La Laguna)",
+                "Juan Montalvo (San Sebastián)",
+                "San Buenaventura"
+        };
+
+        adaptadorPU = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,PU);
+        listaPU.setAdapter(adaptadorPU);
+
+        String[] PR = new String[]{
+                "Toacaso",
+                "San Juan de Pastocalle",
+                "Mulaló",
+                "Tanicuchí",
+                "Guaytacama",
+                "Aláquez",
+                "Poaló",
+                "Once de Noviembre",
+                "Belisario Quevedo",
+                "Joseguango Bajo"
+        };
+
+        adaptadorPR = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,PR);
+        listaPR.setAdapter(adaptadorPR);
+
     }
 
     /**
