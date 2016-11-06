f98a1cd90ca7f894482e6a9b0edf9b2750c85aa9
Bfabarautc <belenfabarav93@gmail.com>
2016-11-06 12:17:00 -0500

    naturales

--- web/GrupoA #1/administracion/app/models/Naturales.java
+++ web/GrupoA #1/administracion/app/models/Naturales.java
@@ -1,5 +1,21 @@
 package models;
 
-public class Naturales {
+import play.db.jpa.Model;
+
+public class Naturales extends Model {
+
+	public String nombre;
+	public String descripcion;
+	public String ubicacion;
+	public String imagen;
+	
+	public Naturales(String nombre, String descripcion,String ubicacion, String imagen) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.ubicacion = ubicacion;
+		this.imagen = imagen;
+	}
 
 }
+
