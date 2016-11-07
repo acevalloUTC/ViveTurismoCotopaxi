201a66dca9e7e3dcb0db0f50d999797e9a2daed0
sizaUTC <stalin.iza115@gmail.com>
2016-11-07 00:14:32 -0500

    se creo la interfaz del canton sigchos
    Se diseño y configuro la clase Sigchos para que muestre la imagen del
    canton y las listas de las parroquias urbanas y rurales

---
+++ mobile/GrupoH/DivisionPoliticaProyecto/app/src/main/java/com/divisionpoliticaproyecto/Sigchos.java
@@ -0,0 +1,62 @@
+package com.divisionpoliticaproyecto;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Sigchos extends AppCompatActivity {
+
+    ListView listaPU, listaPR;
+    ArrayAdapter<String> adaptadorPU;
+    ArrayAdapter<String> adaptadorPR;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sigchos);
+
+        //con estas lineas se visualiza el boton atras de la parte superior izquierda
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        listaPU = (ListView)findViewById(R.id.lstPU);
+        listaPR = (ListView)findViewById(R.id.lstPR);
+
+        String[] PU = new String[]{
+                "Sigchos"
+        };
+
+        adaptadorPU = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,PU);
+        listaPU.setAdapter(adaptadorPU);
+
+        String[] PR = new String[]{
+                "Chugchillán",
+                "Isinliví",
+                "Las Pampas",
+                "Palo Quemado"
+        };
+
+        adaptadorPR = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,PR);
+        listaPR.setAdapter(adaptadorPR);
+
+    }
+
+    /**
+     * con esta funcion se activa el boton atras para que regrese a la anterior activiti
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: //hago un case por si en un futuro agrego mas opciones
+                Log.i("ActionBar", "Atrás!");
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
