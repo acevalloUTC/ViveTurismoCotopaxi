aca8948a3e2d2bf2edfe3f03df79f526811c1847
sizaUTC <stalin.iza115@gmail.com>
2016-11-20 21:40:06 -0500

    Configuracion de Boton Atraz
    Se configuro en la clase CotopaxiDP para que se muestre el boton
    retrocedes en la parte superior izquierda.

--- mobile/GrupoH/DivisionPoliticaProyecto/app/src/main/java/com/divisionpoliticaproyecto/CotopaxiDP.java
+++ mobile/GrupoH/DivisionPoliticaProyecto/app/src/main/java/com/divisionpoliticaproyecto/CotopaxiDP.java
@@ -1,7 +1,10 @@
 package com.divisionpoliticaproyecto;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class CotopaxiDP extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class CotopaxiDP extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cotopaxi_dp);
+
+        //con estas lineas se visualiza el boton atras de la parte superior izquierda
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * con esta funcion se activa el boton atras para que regrese a la anterior activiti
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: //hago un case por si en un futuro agrego mas opciones
+                Log.i("ActionBar", "Atrás!");
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
