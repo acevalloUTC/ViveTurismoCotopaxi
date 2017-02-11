7e7d01531f48040d8db4fdd74d7ddc8873ab9a43
Diego1313 <diego.guachamin8@utc.edu.ec>
2017-02-10 21:34:26 -0500

    GrupoIFinal

---
+++ mobile/GrupoI/Informacion/app/src/main/java/com/example/anderson/informacion/Pangua.java
@@ -0,0 +1,13 @@
+package com.example.anderson.informacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pangua extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pangua);
+    }
+}
